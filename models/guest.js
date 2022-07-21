c76492f24e045cc475781663b88f7814c48df113
luisv7 <luisvillarreal_7@icloud.com>
2022-07-21 00:28:45 -0700

    WIP: created schema

---
+++ models/guest.js
@@ -0,0 +1,17 @@
+///// DEPENDENCIES /////
+const mongoose = require('mongoose');
+
+///// SCHEMA /////
+
+const guestsSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    phone: { type: String, required: true },
+    time: { type: String, required: true },
+    date: { type: Date, default: Date.now(), required: true },
+    table: { type: String },
+    guests: { type: String },
+    email: { type: String },
+    comments: { type: String },
+});
+
+module.exports = mongoose.model("Guest", guestsSchema);
