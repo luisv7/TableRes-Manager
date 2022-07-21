11fa3a13b2b1f94e62edda5dd0aed71f2ca9b2c1
luisv7 <luisvillarreal_7@icloud.com>
2022-07-21 00:30:31 -0700

    WIP: added new Dep - controller

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const express = require('express');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
+const Guest = require('./controller/guests');
 
 ///// INITIALIZATION /////
 const app = express();
@@ -12,15 +13,17 @@ mongoose.connection.on('connected', () => {
     console.log('connected to MongoDB')
 });
 
-///// MIDDLEWEAR /////
+///// MIDDLEWARE /////
 app.use(methodOverride('_method'));
-app.use(express.urlencoded({ extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 ///// ROUTES /////
 app.get('/', (req, res) => {
 });
+
+app.get('/guests', Guest)
 
 const port = process.env.PORT;
 app.listen(port, () => {
