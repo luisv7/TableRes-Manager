03ee99d3d526464dccb5dbc5e3d6fa88c27cd05f
luisv7 <luisvillarreal_7@icloud.com>
2022-07-21 00:38:19 -0700

    boilerplate

---
+++ controller/guests.js
@@ -0,0 +1,21 @@
+///// IMPORT MODEL SCHEMA /////
+const Guest = require('../models/guest');
+
+///// INITIALIZATION /////
+const router = require('express').Router();
+
+///// ROUTES/INDUCES /////
+
+/// INDEX
+
+/// NEW
+
+/// DELETE
+
+/// UPDATE - PUT
+
+/// CREATE - POST
+
+/// EDIT
+
+/// SHOW
