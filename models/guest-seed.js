7998db0e459c3969fcf034cf65f938637c1e336b
luisv7 <luisvillarreal_7@icloud.com>
2022-07-22 01:43:41 -0700

    added seed data

---
+++ models/guest-seed.js
@@ -0,0 +1,23 @@
+const seedData = [
+    {
+        name: "Luis",
+        phone: "123-456-7891",
+        time: { hour: 5, minute: 30, meridiem: "PM" },
+        date: { month: "7", day: "27", year: "2022" },
+        table: "T1",
+        guests: 4,
+        email: "example@email.com",
+        comments: "Birthday Party",
+    },{
+        name: "Lily",
+        phone: "123-456-7891",
+        time: { hour: 6, minute: 30, meridiem: "PM" },
+        date: { month: "7", day: "27", year: "2022" },
+        table: "T2",
+        guests: 6,
+        email: "example@email.com",
+        comments: "Business",
+    }
+];
+
+module.exports = seedData;
