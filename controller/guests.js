dfa9fa7f69f70114a3257522af232112c0363555
luisv7 <luisvillarreal_7@icloud.com>
2022-07-22 01:51:17 -0700

    WIP: added new seed data and some routes

--- controller/guests.js
+++ controller/guests.js
@@ -1,14 +1,33 @@
 ///// IMPORT MODEL SCHEMA /////
 const Guest = require('../models/guest');
+const GuestData = require('../models/guest-seed');
 
 ///// INITIALIZATION /////
 const router = require('express').Router();
 
+router.get('/seed', async (req, res) => {
+    try{
+        await Guest.deleteMany({});
+        await Guest.create(GuestData);
+        res.redirect('/guests');
+    }catch (err) {
+        res.send(err.message);
+    }
+}); 
+
 ///// ROUTES/INDUCES /////
 
 /// INDEX
+router.get('/', (req, res) => {
+    Guest.find({}, (err, guests) => {
+        res.render('index.ejs', { guests });
+    })
+})
 
 /// NEW
+router.get('/new', (req,res) => {
+    res.render('new.ejs', );
+});
 
 /// DELETE
 
@@ -19,3 +38,5 @@ const router = require('express').Router();
 /// EDIT
 
 /// SHOW
+
+module.exports = router;
